06ad80883ad08a58acf4fca74d3f66ed8e2ead10
fmflurry <fm.florian.michel@gmail.com>
2025-12-05 14:42:31 +0100

    feat: add json-patch POC implementation

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { Address } from "./address.model";
+import { Person } from "./person.model";
+import { compare } from "fast-json-patch";
+
+const address: Address = {
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+const newAddress: Address = {
+  street: "456 Elm St",
+  city: "Randomtown",
+  state: "FL",
+  zip: "67890",
+};
+
+const john = {
+  name: "John Doe",
+  age: 30,
+  address: address,
+};
+
+const patch = compare({}, john);
+
+console.log("👨‍💻 John : ", john);
+console.log(" --- ");
+console.log("📦 Patch : ", patch);
+
+const updatedJohn = {
+  ...john,
+  address: newAddress,
+};
+
+console.log("〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️");
+console.log("👨‍💻 Updated John : ", updatedJohn);
+console.log(" --- ");
+console.log("📦 Updated Patch : ", compare(john, updatedJohn));
---
+++ src/person.model.ts
@@ -0,0 +1,7 @@
+import { Address } from "./address.model";
+
+export interface Person {
+  name: string;
+  age: number;
+  address: Address;
+}
